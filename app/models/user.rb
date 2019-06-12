3b88518296937c978326f0aa00e88595cd73dcb4
Junichi Ito <jit@sonicgarden.jp>
2019-06-13 04:49:52 +0900

    Add tests for Devise class methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, validate_on_invite: true
 
   validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "With valid params and class method" do
+    user = User.invite!(email: 'new_user@example.com', name: 'John Doe')
+    assert user.persisted?
+  end
+
+  test "With invalid params and class method" do
+    user = User.invite!(email: 'new_user@example.com', name: '')
+    refute user.persisted?
+  end
 end
